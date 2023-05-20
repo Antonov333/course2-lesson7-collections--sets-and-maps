0fe41a8175a4d3e2326171e3bf6c48d08492386d
Antonov333 <antonovsergei333@yahoo.com>
2023-05-20 23:06:31 +0300

    Problem #1 solved

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,28 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Course 2, Lesson 7 \"COLLECTIONS: SETS AND MAPS");
+        System.out.println("Course 2, Lesson 7 \"COLLECTIONS: SETS AND MAPS\"");
+
+        List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+
+        printNonEvenNumbers(nums);
+
     }
+
+    public static void printNonEvenNumbers(List<Integer> list) {
+        System.out.println("\nProblem #1\n" +
+                "----------");
+        for (Integer i : list) {
+            if (i % 2 == 1) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println("");
+
+    }
+
 }
